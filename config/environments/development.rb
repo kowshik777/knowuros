9425beaf107af206e1d0e17a0ccdc3ad5310331a
kowshik007 <kowshikkondapaneni@gmail.com>
2015-06-26 14:39:20 +0530

    make it better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,13 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   # Needed for devise
-  config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => 'kowshik16.kk@gmail.com',
-  :password             => 'KK1234567',
-  :authentication       => 'login',
-  :enable_starttls_auto => true  }
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 end
