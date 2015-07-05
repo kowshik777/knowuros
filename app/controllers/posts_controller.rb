d558db84d2658900c4eedf808bc116fa1e8c4830
kowshik007 <kowshikkondapaneni@gmail.com>
2015-07-05 13:27:51 +0530

    make it better

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
   if params[:category].blank?
-    @posts = Post.search(params[:search]).paginate(page: params[:page], per_page: 20)
+    @posts = Post.search(params[:search]).paginate(page: params[:page], per_page: 2)
   else
     @category_id = Category.find_by(name: params[:category]).id
     @posts = Post.where(category_id: @category_id).paginate(page: params[:page], per_page: 20)
