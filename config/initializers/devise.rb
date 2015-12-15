44770c39fdd3d97caf1eaeae57886b2eb310b83e
kowshik777 <kowshik16.kk@gmail.com>
2015-12-15 15:49:35 +0530

    make it better

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config|
-  config.mailer_sender = 'kowshik16.kk@gmail.com'
+  config.mailer_sender = 'knowuros@gmx.com'
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
