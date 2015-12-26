f2cdf4d63d833b4e3a376cb22ebcf8d0f6676463
kowshik777 <kowshik16.kk@gmail.com>
2015-12-26 10:12:21 +0530

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   config.assets.compile = false
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'knowuros.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'peaceful-meadow-5940.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address              => "mail.gmx.com",
