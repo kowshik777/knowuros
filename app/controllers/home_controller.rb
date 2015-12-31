ccce5dded332153594f73b24c5c32fe891fe1058
kowshik777 <kowshik16.kk@gmail.com>
2015-12-31 10:23:47 +0530

    make it better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@title = "Know Your Operating System"
-  	@meta_description = "Know your operating system. This website tells you about the advanced features of your operating system"
+  	@title = "Know Your Operating System windows & linux for pc"
+  	@meta_description = "Know your operating system. This website tells you about windows operating system thus windows 7, 8 and 10 and linux operating system ubuntu."
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
-  config.action_mailer.default_url_options = { :host => 'peaceful-meadow-5940.herokuapp.com' } 
+  config.action_mailer.default_url_options = { :host => 'knowuros.herokuapp.com' } 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
